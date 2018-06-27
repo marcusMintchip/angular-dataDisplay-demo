34df8b0c1a24cc7f1db177d707ed575f35d16046
cxwang <cxwang@ittx.com.cn>
2018-06-27 18:32:27 +0800

    第一次提交angular项目

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'tour of hero';
+  heroes = [new Hero(1,'windstorm'),new Hero(2,'bombshell'),new Hero(3,'alexa')];
+  myHero = this.heroes[0];
 }
